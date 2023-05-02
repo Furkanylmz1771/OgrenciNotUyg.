7421e9f3c3ba425d27c161f735bc7f22a5acdb5d
Furkan Yılmaz <92302477+Furkanylmz1771@users.noreply.github.com>
2023-05-02 21:05:24 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using Ogrenci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form
+{
+    public partial class Form1 : Ogrenci
+    {
+        private readonly object MessageBox;
+        public Form1() {
+
+            InitializeComponent();  // Nesneye  ait olan üye elemanlarla(button, label, textbox vs) ilgili ilk işlemler yapılır.
+
+        }
+
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void Form1.load(object sender, EventArgs e)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Adi = "Furkan";
+            ogr.SoyAdi = "Yılmaz";
+            ogr.Alani = Alan.Sözel;
+
+
+            Ders d = new Ders { Adi = "Matematik", Vize = 50, Final = 30 };
+            Ders d2 = new Ders { Adi = "Kimya", Vize = 80,Final=40 };
+
+
+            ogr.Dersler.Add(d);
+            ogr.Dersler2.Add();
+            int durum = (int)ogr.durum;
+
+
+            switch (ogr.Durum)
+            {
+                case OgrenciDurum.Gecti:
+                    MessageBox.Show("Sınıfı Geçtiniz");
+                    break;
+
+                case OgrenciDurum.Kaldı:
+                    MessageBox.Show("Sınıfta kaldınız");
+                    break;
+
+                case OgrenciDurum.ŞartlıGecti:
+                    MessageBox.Show("ŞartlıGeçtiniz");
+                    break;
+
+                case OgrenciDurum.Devamsız:
+                    MessageBox.Show(" Devamsızlıktan kaldınız");
+                    break;
+
+                case OgrenciDurum.Tekrar:
+                    MessageBox.Show("Tekrar");
+                    break;
+            }
+
+        }
+
+
+    }
+  
+}   
---
+++ Okul.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrenci
+{
+    class Ogrenci
+    {
+        public Ogrenci()
+        {
+            Dersler = new List<Ders>();
+        }
+        public string Adi
+        {
+            get; set;
+
+        }
+        public string SoyAdi { get; set; }
+
+        public Alan Alani { get; set; }
+
+        public List<Ders> Dersler { get; set; }       
+        
+        public decimal Ortalama
+        {
+            get
+            {
+                decimal toplam = 0;
+                foreach (Ders not in Dersler)
+                {
+                    toplam += not.Ortalama;
+                }
+                return toplam / Dersler.Count();
+            }
+        }
+    }
+
+    public OgrenciDurum Durum
+    {
+        get
+        {
+            if (Ortalama >= 70)
+            {
+                return OgrenciDurum.Gecti;
+            }else if(Ortalama >= 50)
+            {
+                return OgrenciDurum.ŞartlıGecti;
+            }
+            else
+            {
+                return OgrenciDurum.Kaldı;
+            }
+        }
+    }
+
+
+    struct Ders 
+    {
+
+        public string Adi
+        {
+            get; set;
+        }
+
+        public decimal Vize
+        {
+            get; set;
+        }
+        public decimal Final { get; set; }
+
+        public decimal Ortalama
+        {
+            get
+            {
+                return Vize * 0.4m + Final * 0.6m;  // Decimal için m
+            }
+        }
+    
+    }
+
+    enum OgrenciDurum           // enum bir seçenek listesidir. Programcının nesne ile ilgili durumları kontrol etmesini sağlar.
+    {
+        Gecti,
+        Kaldı,
+        ŞartlıGecti,
+        Devamsız,
+        Tekrar
+    }
+
+    enum Alan
+    {
+        Sayisal,
+        Sözel,
+        EşitAgırlık,
+        Dil
+
+    }
+
+
+}
